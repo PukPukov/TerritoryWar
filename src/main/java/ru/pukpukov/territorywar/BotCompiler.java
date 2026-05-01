8ed7c062cecce6c13ebce6b4746de8d89f183062
PukPukov <kvasov.les@gmail.com>
2026-05-01 20:14:54 +0500

    ...

--- src/main/java/ru/pukpukov/territorywar/BotCompiler.java
+++ src/main/java/ru/pukpukov/territorywar/BotCompiler.java
@@ -96,7 +96,7 @@ public class BotCompiler {
         
         // Загружаем скомпилированный класс из нужной директории
         try (URLClassLoader classLoader = new URLClassLoader(new URL[]{tempDir.toURI().toURL()})) {
-            return (Class<? extends Bot>) Class.forName("com.territorywar." + className, true, classLoader);
+            return (Class<? extends Bot>) Class.forName("ru.pukpukov.territorywar." + className, true, classLoader);
         }
     }
 }
